624ca4113f05b730e7d9ae1cacf86593600ad3cc
Applewithmayo <ed.maldonadoe@gmail.com>
2023-02-05 18:58:30 -0300

    Update home and about me sections

--- main.js
+++ main.js
@@ -15,10 +15,23 @@ if (navToggle){
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu');
+    });
+}
 
 
 /*==================== REMOVE MENU MOBILE ====================*/
 
+const navLink = document.querySelectorAll('.nav__link');
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    //Cuando se haga clic en un nav link, se quitara el menu mobile
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*==================== ACCORDION SKILLS ====================*/
 
